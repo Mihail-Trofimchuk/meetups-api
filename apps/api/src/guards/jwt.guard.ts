dd2a5e7bbc2a93dfad37f827020704743eb18a6a
Mikhail <mihatr2002@gmail.com>
2023-10-05 00:56:59 +0300

    add jwt strategy

--- apps/api/src/guards/jwt.guard.ts
+++ apps/api/src/guards/jwt.guard.ts
@@ -1,3 +1,5 @@
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-export class JWTAuthGuard extends AuthGuard('jwt') {}
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {}
